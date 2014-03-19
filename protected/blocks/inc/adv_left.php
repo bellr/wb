8de7c9bc350dbb713e6517afb4d34d02edb63d37
AtomLy <atomly@mail.ru>
2014-03-19 12:09:39 +0300

    add widget

--- protected/blocks/inc/adv_left.php
+++ protected/blocks/inc/adv_left.php
@@ -3,14 +3,16 @@ class adv_left extends Template {
 
     public function block() {
 		$banners = array(
-		'banner-200x200-europe',
-			'banner-200x300-rebate',
-			'insta_banner_iframe_200_200',
-			'banner-200x200-ForexStart',
-			'insta_banner_iframe_200_400',
-			'banner-200x300-Rouble');
+		    //'banner-200x200-europe',
+			//'banner-200x300-rebate',
+			//'insta_banner_iframe_200_200',
+			//'banner-200x200-ForexStart',
+			//'insta_banner_iframe_200_400',
+			//'banner-200x300-Rouble',
+            'kassir'
+        );
 
-		$b = rand(0,5);
+		$b = rand(0,count($banners)-1);
 		$this->tmplName = $banners[$b];
         return $this;
     }
