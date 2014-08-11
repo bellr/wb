079fcd5924237caec6e357651c03e782cddbc028
AtomLy <atomly@mail.ru>
2014-08-11 08:27:34 +0300

    feature: git ignor header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 define('PROJECT','HOME');
 define('PROJECT_ROOT',dirname(__FILE__));
-define('VS_DEBUG',true);
+define('VS_DEBUG',false);
 date_default_timezone_set("Europe/Kiev");
 
 require_once(dirname(PROJECT_ROOT)."/core/vs.php");
